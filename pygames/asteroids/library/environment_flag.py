89845e236b4aa710f0acfcbc55031f52efde57c5
Mike Ton <mike.ton@gmail.com>
2023-04-13 10:31:40 -0700

    code review pass on environment_flag and unit test

--- pygames/asteroids/library/environment_flag.py
+++ pygames/asteroids/library/environment_flag.py
@@ -9,6 +9,8 @@ class EnvironmentFlag:
 
     CONFIG_FILE_PATH: str = "config.ini"
     SECTION_FLAGS: str = "Flags"
+    IS_PRODUCTION_FLAG: str = "IS_PRODUCTION_BUILD"
+    IS_MAC_BUILD_FLAG: str = "IS_MAC_BUILD"
     # default values if config file is missing or malformed
     IS_PRODUCTION_DEFAULT: bool = False
     IS_MAC_BUILD_DEFAULT: bool = False
@@ -18,24 +20,31 @@ class EnvironmentFlag:
         # set default values to False
         self.is_production = EnvironmentFlag.IS_PRODUCTION_DEFAULT
         self.is_mac_build = EnvironmentFlag.IS_MAC_BUILD_DEFAULT
+        self._read_config(config_file_path)
 
+    def _read_config(self, config_file_path: str):
         try:
             config_content = self.config.read(config_file_path)
             if config_content == []:
-                logging.warning("No config file found at {config_file_path}")
+                logging.warning(f"No config file found at {config_file_path}")
                 return
             if self.has_section():
                 try:
                     self.is_production = self.config.getboolean(
-                        EnvironmentFlag.SECTION_FLAGS, "IS_PRODUCTION_BUILD"
+                        EnvironmentFlag.SECTION_FLAGS,
+                        EnvironmentFlag.IS_PRODUCTION_FLAG,
                     )
                     self.is_mac_build = self.config.getboolean(
-                        EnvironmentFlag.SECTION_FLAGS, "IS_MAC_BUILD"
+                        EnvironmentFlag.SECTION_FLAGS, EnvironmentFlag.IS_MAC_BUILD_FLAG
                     )
                 except configparser.NoOptionError as e:
                     logging.warning(f"Missing option in config file : {e}")
+                except ValueError as e:
+                    raise InvalidFlagValueError("Invalid value in config file : {e}")
             else:
-                logging.warning("No SECTION HEADER found in config file")
+                logging.warning(
+                    f"No SECTION HEADER found in config file at {config_file_path}"
+                )
         except configparser.MissingSectionHeaderError as e:
             logging.warning(f"Missing SECTION HEADER in config file : {e}")
             # set default values in case of an error
@@ -44,3 +53,11 @@ class EnvironmentFlag:
 
     def has_section(self) -> bool:
         return self.config.has_section(EnvironmentFlag.SECTION_FLAGS)
+
+
+class InvalidFlagValueError(ValueError):
+    """
+    Exception raised when an invalid option value is passed to a flag
+    """
+
+    pass
