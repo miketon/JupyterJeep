4f69d523c6dff2c450ab970c8fe9d0d9fa890817
Mike Ton <mike.ton@gmail.com>
2023-04-15 08:56:56 -0700

    Adding simple visit counter

--- html/app_hello_world_flask.py
+++ html/app_hello_world_flask.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+visit_count = 0
 
 @app.route('/')
 def home():
-    return "Hello, world!"
+    global visit_count
+    visit_count += 1
+    return f"Hello, world! This page has been visited {visit_count} times."
 
 if __name__ == "__main__":
     app.run(debug=True)
