2e9df43d435f6fb59c2facb6b054fb393a1a8862
Mike Ton <mike.ton@gmail.com>
2023-04-22 16:15:37 -0700

    Updating the .gitignore file to ignore the target

---
+++ rusty_jeep_project/target/debug/deps/rusty_jeep_project-41092dc2ec2b1c8e.d
@@ -0,0 +1,5 @@
+/Users/mton/Documents/GitHub/JupyterJeep/rusty_jeep_project/target/debug/deps/rusty_jeep_project-41092dc2ec2b1c8e.rmeta: src/main.rs
+
+/Users/mton/Documents/GitHub/JupyterJeep/rusty_jeep_project/target/debug/deps/rusty_jeep_project-41092dc2ec2b1c8e.d: src/main.rs
+
+src/main.rs:
---
+++ rusty_jeep_project/target/debug/rusty_jeep_project.d
@@ -0,0 +1,1 @@
+/Users/mton/Documents/GitHub/JupyterJeep/rusty_jeep_project/target/debug/rusty_jeep_project: /Users/mton/Documents/GitHub/JupyterJeep/rusty_jeep_project/src/main.rs
