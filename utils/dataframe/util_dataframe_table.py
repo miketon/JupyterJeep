8969b5663a0a1fa311f29a35159b1a76075a3170
Mike Ton <mike.ton@gmail.com>
2023-04-23 16:57:54 -0700

    unit test yet another pass

--- utils/dataframe/util_dataframe_table.py
+++ utils/dataframe/util_dataframe_table.py
@@ -4,12 +4,15 @@ import pandas as pd
 import torch
 from typing import Optional, Callable, Union
 from IPython.display import display
+import logging
 
 
 class UtilDataFrameTable:
     """
     Utility class to generate and display a DataFrame table
     """
+
+    PADDING_DEFAULT: int = 0
 
     @staticmethod
     def show_table(
@@ -21,6 +24,10 @@ class UtilDataFrameTable:
         Generates a DataFrame table and display() it given a tensor and map function
         :param data: torch.Tensor or pd.DataFrame (to visualize tables that have strings)
         """
+        if not isinstance(data, (torch.Tensor, pd.DataFrame)):
+            raise ValueError("data must be a torch.Tensor or pd.DataFrame")
+        elif len(data.shape) < 2:
+            raise ValueError("data.shape has to be at least 2D")
 
         if label is not None:
             print(f"--[{label}]--")
@@ -39,7 +46,7 @@ class UtilDataFrameTable:
         return True
 
     @staticmethod
-    def array_to_table(data: np.ndarray) -> np.ndarray:
+    def array_to_table(data: np.ndarray, pad_value="") -> np.ndarray:
         """
         Convert a 1D array to balanced 2D table
         :param data: 1D Numpy array
@@ -51,6 +58,12 @@ class UtilDataFrameTable:
         data_sq_x = data_sq_y = int(math.ceil(math.sqrt(total_elements)))
         mod = total_elements % data_sq_x
         if mod != 0:
+            # default pad value is 0
+            if not UtilDataFrameTable._is_convertible_to_int(pad_value):
+                logging.warning(
+                    f"pad_value {pad_value} is not convertible to int : setting to default {UtilDataFrameTable.PADDING_DEFAULT}"
+                )
+                pad_value = UtilDataFrameTable.PADDING_DEFAULT
             # Calculate the next perfect square
             # Pad the input array with zeros
             padding = (data_sq_x * data_sq_y) - total_elements
@@ -58,8 +71,18 @@ class UtilDataFrameTable:
                 padding = padding - data_sq_x
                 data_sq_x = data_sq_x - 1
 
-            data = np.pad(data, (0, np.abs(padding)), "constant", constant_values=0)
+            data = np.pad(
+                data, (0, np.abs(padding)), "constant", constant_values=pad_value
+            )
 
         # reshape the padded array to the desired dimension
         table = data.reshape(data_sq_x, data_sq_y)
         return table
+
+    @staticmethod
+    def _is_convertible_to_int(x):
+        try:
+            int(x)
+            return True
+        except ValueError:
+            return False
