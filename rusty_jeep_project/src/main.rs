681c80b6f77baadc29265ae2377aa079fcf6d98e
Mike Ton <mike.ton@gmail.com>
2023-04-24 05:59:51 -0700

    rusty guessing game

--- rusty_jeep_project/src/main.rs
+++ rusty_jeep_project/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world! I'm a Rusty Jeep!");
+    print!("Guess the number!");
+    print!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    print!("You guessed : {guess}")
+
 }
