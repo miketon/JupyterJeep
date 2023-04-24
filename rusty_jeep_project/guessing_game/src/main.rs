7b889ba81fb65fbf6229cb57e520b2a200a38b0d
Mike Ton <mike.ton@gmail.com>
2023-04-24 12:04:54 -0700

    rusty guessing game hello world

--- rusty_jeep_project/guessing_game/src/main.rs
+++ rusty_jeep_project/guessing_game/src/main.rs
@@ -1,4 +1,15 @@
-
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // by default rust variables are immutable
+    // mut keyword makes it mutable
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    print!("You guessed: {guess}");
 }
