01c1510ff03c3026b4ee08f04eaa1cbb2606cdce
Mike Ton <mike.ton@gmail.com>
2023-04-24 15:30:36 -0700

    compressing systems to plugin

--- rusty_jeep_project/bevy_game_scratchpad/src/main.rs
+++ rusty_jeep_project/bevy_game_scratchpad/src/main.rs
@@ -3,13 +3,11 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugin(HelloPlugin)
         .run();
 }
 
-fn hello_world(){
+fn hello_world() {
     println!("Hello world! Bevy birds are NOT angry!");
 }
 
@@ -19,14 +17,26 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn add_people(mut commands:Commands){
+fn add_people(mut commands: Commands) {
+    println!("Adding people...");
     commands.spawn((Person, Name("Mario".to_string())));
     commands.spawn((Person, Name("Luigi".to_string())));
     commands.spawn((Person, Name("Peach".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>){
-    for name in &query{
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
         println!("Hello {}!", name.0);
+    }
+}
+
+// Moving our systems into a plugin
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_startup_system(hello_world)
+            .add_system(greet_people); // why does this not print as add_startup_system?
     }
 }
