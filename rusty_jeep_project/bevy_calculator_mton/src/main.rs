12d3fc75dd4bd7fb2a4cbb61412e3b40b4aa4d1d
Mike Ton <mike.ton@gmail.com>
2023-04-25 15:41:50 -0700

    Window loading

--- rusty_jeep_project/bevy_calculator_mton/src/main.rs
+++ rusty_jeep_project/bevy_calculator_mton/src/main.rs
@@ -1,20 +1,59 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, winit::WinitSettings};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy Calculator Mton".into(),
-                resolution: (320., 512.).into(),
-                fit_canvas_to_parent: true,
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_system(hello_world)
+        .add_plugins(DefaultPlugins)
+        // Reduce CPU/GPU usage : Only run app when there is user input
+        .insert_resource(WinitSettings::desktop_app())
+        .add_startup_system(hello_world)
+        .add_startup_system(setup_calc_ui)
         .run();
 }
 
 fn hello_world() {
     println!("Hello, world! BEVY CALCULATOR MTON");
+}
+
+struct ButtonMaterials {
+    normal: Handle<ColorMaterial>,
+    hovered: Handle<ColorMaterial>,
+    pressed: Handle<ColorMaterial>,
+    cc: Handle<ColorMaterial>,
+}
+
+impl FromWorld for ButtonMaterials {
+    fn from_world(world: &mut World) -> Self {
+        let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
+        ButtonMaterials {
+            normal: materials.add(Color::rgb(0.02, 0.02, 0.02).into()),
+            hovered: materials.add(Color::rgb(0.12, 0.12, 0.12).into()),
+            pressed: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
+            cc: materials.add(Color::rgb(0.75, 0.0, 0.0).into()),
+        }
+    }
+}
+
+/*
+fn button_system(
+    button_materials: Res<ButtonMaterials>,
+    mut interaction_query: Query<
+        (&Interaction, &mut Handle<ColorMaterial>),
+        (Changed<Interaction>, With<Button>),
+    >
+) {
+
+}
+*/
+
+fn setup_calc_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(NodeBundle {
+        style: Style {
+            size: Size::width(Val::Percent(100.0)),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        ..default()
+    });
 }
