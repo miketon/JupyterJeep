1bd3f6234aa0a45d2e76a6b3e34bcd88fca98deb
Mike Ton <mike.ton@gmail.com>
2023-04-27 14:09:34 -0700

    gpt code reviewed improvements

--- rusty_jeep_project/bevy_calculator_mton/src/button.rs
+++ rusty_jeep_project/bevy_calculator_mton/src/button.rs
@@ -6,14 +6,44 @@ pub struct ButtonColors {
     pub pressed: Color,
 }
 
-impl ButtonColors {
-    pub fn new() -> Self {
+impl Default for ButtonColors {
+    fn default() -> Self {
         ButtonColors {
             normal: Color::rgb(0.2, 0.2, 0.2),
             hovered: Color::rgb(0.4, 0.4, 0.4),
             pressed: Color::rgb(0.9, 0.9, 0.6),
         }
     }
+}
+
+pub fn generate_button() -> ButtonBundle {
+    ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+            padding: UiRect {
+                left: Val::Px(10.0),
+                right: Val::Px(10.0),
+                top: Val::Px(10.0),
+                bottom: Val::Px(10.0),
+            },
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        background_color: ButtonColors::default().normal.into(),
+        ..default()
+    }
+}
+
+pub fn generate_text(asset_server: &AssetServer) -> TextBundle {
+    TextBundle::from_section(
+        "Hello World",
+        TextStyle {
+            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+            font_size: 40.0,
+            color: Color::WHITE,
+        },
+    )
 }
 
 pub fn button_system(
@@ -31,7 +61,7 @@ pub fn button_system(
 }
 
 fn update_button(interaction: &Interaction, text: &mut Text, color: &mut BackgroundColor) {
-    let button_colors = ButtonColors::new();
+    let button_colors = ButtonColors::default();
     match *interaction {
         Interaction::Clicked => {
             text.sections[0].value = "Clicked".to_string();
--- rusty_jeep_project/bevy_calculator_mton/src/main.rs
+++ rusty_jeep_project/bevy_calculator_mton/src/main.rs
@@ -11,14 +11,8 @@ fn main() {
             }),
             ..default()
         }))
-        // .init_resource::<Configuration>() // `ResourceInspectorPlugin` won't initialize the resource
-        // .register_type::<Configuration>() // you need to register your type to display it       .add_plugin(ResourceInspectorPlugin::<Configuration>::default())
-        // also works with built-in resources, as long as they implement `Reflect`
-        // .add_plugin(ResourceInspectorPlugin::<Time>::default())
-        // .add_plugins(WorldInspectorPlugin::new('WorldInspectorPlugin'))
         // Reduce CPU/GPU usage : Only run app when there is user input
         .insert_resource(WinitSettings::desktop_app())
-        // .init_resource::<ButtonMaterials>()
         .add_startup_system(hello_world)
         .add_startup_system(setup_calc_ui)
         .add_system(button::button_system)
@@ -34,43 +28,45 @@ fn hello_world(mut local_counter: Local<i32>) {
 }
 
 fn setup_calc_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let button_colors = button::ButtonColors::new();
     let canvas_node = NodeBundle {
         style: Style {
-            size: Size::width(Val::Percent(100.0)),
+            size: Size {
+                width: Val::Percent(90.0),
+                height: Val::Percent(90.0),
+            },
+            flex_direction: FlexDirection::Row,
+            flex_wrap: FlexWrap::Wrap,
+            align_self: AlignSelf::FlexStart,
+            justify_content: JustifyContent::SpaceAround,
             align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
             ..default()
         },
-        background_color: Color::rgb(0.0, 0.5, 0.0).into(),
+        background_color: Color::rgb(0.5, 0.5, 0.5).into(),
         ..default()
     };
 
-    let button_node = ButtonBundle {
-        style: Style {
-            size: Size::new(Val::Px(200.0), Val::Px(100.0)),
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-            ..default()
-        },
-        background_color: button_colors.normal.into(),
-        ..default()
-    };
-    let text_node = TextBundle::from_section(
-        "Hello World",
-        TextStyle {
-            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-            font_size: 40.0,
-            color: Color::WHITE,
-        },
-    );
+    // let button_node = button::generate_button();
+    let text_node = button::generate_text(&asset_server);
 
     // Spawan a camera
     commands.spawn(Camera2dBundle::default());
     // setup ui hierarchy
     commands.spawn(canvas_node).with_children(|parent| {
-        parent.spawn(button_node).with_children(|parent| {
-            parent.spawn(text_node);
-        });
+        populate_button_grid(parent, text_node);
+        // parent.spawn(button_node).with_children(|parent| {
+        //     parent.spawn(text_node);
+        // });
     });
+}
+
+fn populate_button_grid(parent: &mut ChildBuilder, text_node: TextBundle) {
+    let button_symbols = vec!["0", "*", "/", "="];
+    for i in button_symbols {
+        println!("{}", i);
+        parent
+            .spawn(button::generate_button())
+            .with_children(|parent| {
+                parent.spawn(text_node.clone());
+            });
+    }
 }
