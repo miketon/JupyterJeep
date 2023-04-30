9ddb0dfc783a342f5f1d6e74e432308b9091e4d9
Mike Ton <mike.ton@gmail.com>
2023-04-30 15:18:32 -0700

    unit test scratch pad variant exercise drafts

--- rusty_jeep_project/unit_test_scratchpad/src/lib.rs
+++ rusty_jeep_project/unit_test_scratchpad/src/lib.rs
@@ -10,7 +10,27 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    // Written to return Result instead of panicking
+    // Writing tests so they return a Result<T, E> enables you to use the
+    // question mark operator in the body of tests, which can be a convenient
+    // way to write tests that should fail if any operation within them returns
+    // an Err variant.
+    // You can’t use the #[should_panic] annotation on tests that use
+    // Result<T, E>. To assert that an operation returns an Err variant,
+    // don’t use the question mark operator on the Result<T, E> value.
+    // Instead, use assert!(value.is_err()).
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    // Checking if expected string contains "less than or equal to 100"
+    // @note This makes our PANIC check MORE EXPLICIT and SPECIFIC
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
@@ -95,8 +115,11 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}");
+        // if value < 1 || value > 100 {
+        if value < 1 {
+            panic!("Guess value must less than or equal to 100, got {value} ");
+        } else if value > 100 {
+            panic!("Guess value must be greater than or equal to 1, got {value}");
         }
         Guess { value }
     }
