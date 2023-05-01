2365c8822c0eef5e6e5cded8752857811f13164a
Mike Ton <mike.ton@gmail.com>
2023-05-01 15:56:41 -0700

    idiomatic : convert parse to Config::new()

--- rusty_jeep_project/minigrep/src/main.rs
+++ rusty_jeep_project/minigrep/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // export from main to sub function because
     // - subdivides scope to RELEASE closure only LOCAL variables
     // - makes it easier to test, enforces single responsibility
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     // the following output will be ...
     println!("Searching for {}", config.query); // Searching for needle
@@ -29,23 +29,26 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    // &args[0] is the name of the program, we want the FIRST argument
-    // There’s a tendency among many Rustaceans to avoid using clone to fix 
-    // ownership problems because of its runtime cost. In Chapter 13, you’ll 
-    // learn how to use more efficient methods in this type of situation. 
-    // But for now, it’s okay to copy a few strings to continue making progress 
-    // because you’ll make these copies only once and your file path and query 
-    // string are very small. 
-    // It’s better to have a working program that’s a bit inefficient than to 
-    // try to hyperoptimize code on your first pass. As you become more 
-    // experienced with Rust, it’ll be easier to start with the most efficient 
-    // solution, but for now, it’s perfectly acceptable to call clone.
-    // @todo : learn more about clone, keep it simple for now
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        // &args[0] is the name of the program, we want the FIRST argument
+        // There’s a tendency among many Rustaceans to avoid using clone to fix
+        // ownership problems because of its runtime cost. In Chapter 13, you’ll
+        // learn how to use more efficient methods in this type of situation.
+        // But for now, it’s okay to copy a few strings to continue making progress
+        // because you’ll make these copies only once and your file path and query
+        // string are very small.
+        // It’s better to have a working program that’s a bit inefficient than to
+        // try to hyperoptimize code on your first pass. As you become more
+        // experienced with Rust, it’ll be easier to start with the most efficient
+        // solution, but for now, it’s perfectly acceptable to call clone.
+        // @todo : learn more about clone, keep it simple for now
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    // updating to explicitly return a Config struct
-    // as opposed to a implicity tuple
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
