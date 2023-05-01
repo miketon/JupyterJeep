a24f49161a7811c72d7ff50eb6fa068c8d5e8c27
Mike Ton <mike.ton@gmail.com>
2023-05-01 08:19:07 -0700

    adding helpful .txt read me notes

--- rusty_jeep_project/unit_test_scratchpad/tests/integration_test.rs
+++ rusty_jeep_project/unit_test_scratchpad/tests/integration_test.rs
@@ -1,6 +1,14 @@
 use unit_test_scratchpad;
+mod common;
 
 #[test]
 fn it_adds_two(){
+    common::setup();
     assert_eq!(4, unit_test_scratchpad::add_two(2));
+}
+
+#[test]
+#[ignore]
+fn an_expensive_integration_test(){
+    panic!("ETA test COMPLETES in the next ~100 years")
 }
