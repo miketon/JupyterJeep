1cfc34a76a8afc5dc545e24578818a12678bd9ca
Mike Ton <mike.ton@gmail.com>
2023-05-02 14:32:10 -0700

    Stubbing out search_case_insensitve FAIL test

--- rusty_jeep_project/minigrep/src/lib.rs
+++ rusty_jeep_project/minigrep/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, vec};
 
 pub struct Config {
     pub query: String,
@@ -77,16 +77,39 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitve<'a>(_query: &str, _contents: &'a str)-> Vec<&'a str>{
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
+        // ensure that we don’t accidentally break the case-sensitive search 
+        // functionality that we’ve already implemented, but adding :
+        // - "Duct tape." to the contents
+        // using a capital D that shouldn’t match the query "duct" when we’re 
+        // searching in a case-sensitive manner
+        // So that we can test both cases, we’ll add a new test function that
+        // calls search with a case-sensitive query and assert that the search
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitve(query, contents));
     }
 }
