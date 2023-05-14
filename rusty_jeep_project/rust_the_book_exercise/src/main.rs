ddcb52412071ce23bae67359cd83c1641249edf0
Mike Ton <mike.ton@gmail.com>
2023-05-14 13:30:29 -0700

    compiler capturing

--- rusty_jeep_project/rust_the_book_exercise/src/main.rs
+++ rusty_jeep_project/rust_the_book_exercise/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    let a = 5;
+    let b = 7;
+    let _result = a + b;
+}
+
+/*
+fn main() {
     println!("[main.rs] on awake");
     on_main_print_greeting();
 }
@@ -6,3 +13,4 @@ fn main() {
 fn on_main_print_greeting() {
     println!("[on_main_print_greeting] Heyao World!!!");
 }
+*/
