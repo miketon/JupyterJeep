a2c6252fe64e3b9ac37d93ec981f80d550e0c8e5
Mike Ton <mike.ton@gmail.com>
2023-06-08 14:31:00 -0700

    Update to use hashmap pass

--- rusty_jeep_project/bevy_inspector_gui/examples/dock_plugin/mod.rs
+++ rusty_jeep_project/bevy_inspector_gui/examples/dock_plugin/mod.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{bevy_egui::EguiContexts, bevy_egui::EguiPlugin, egui, egui::Context};
+use core::panic;
+use std::collections::HashMap;
 use std::sync::Arc;
 
 enum PanelBuildType {
@@ -41,26 +43,24 @@ impl Default for IsVisible {
 
 // @note : Add a field to `DockPlugin` to store the closure:
 pub struct DockPlugin {
-    left_dock_widgets: Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>,
+    panel_builders: HashMap<String, Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>>,
 }
 // @note :  Update the `DockPlugin` implementation to store the closure in the plugin
 impl DockPlugin {
-    pub fn new(left_dock_widgets: impl Fn(&mut egui::Ui) + Send + Sync + 'static) -> Self {
-        DockPlugin {
-            left_dock_widgets: Arc::new(left_dock_widgets),
-        }
+    pub fn new(
+        panel_builders: HashMap<String, Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>>,
+    ) -> Self {
+        DockPlugin { panel_builders }
     }
 }
 
 impl Plugin for DockPlugin {
     fn build(&self, app: &mut App) {
-        let left_dock_widgets = self.left_dock_widgets.clone();
+        let panel_builders = self.panel_builders.clone();
 
         app.add_plugin(EguiPlugin)
             .insert_resource(IsVisible::default())
-            .insert_resource(DrawDockParams {
-                left_dock_widgets: left_dock_widgets.clone(),
-            })
+            .insert_resource(DrawDockParams { panel_builders })
             .add_system(toggle_dock)
             .add_system(draw_dock);
     }
@@ -68,13 +68,13 @@ impl Plugin for DockPlugin {
 
 #[derive(Resource)]
 struct DrawDockParams {
-    left_dock_widgets: Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>,
+    panel_builders: HashMap<String, Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>>,
 }
 
 impl Default for DrawDockParams {
     fn default() -> Self {
         Self {
-            left_dock_widgets: Arc::new(|_| {}),
+            panel_builders: HashMap::new(),
         }
     }
 }
@@ -101,32 +101,36 @@ fn toggle_dock(mut is_visible: ResMut<IsVisible>, key_input: Res<Input<KeyCode>>
 /// - o_space: ResMut<OccupiedSpace>    // occupied space
 /// - is_visible: Res<IsVisible>        // is visible
 /// - params: Res<DrawDockParams>       // draw dock params
-fn draw_dock(
-    mut contexts: EguiContexts,
-    mut o_space: Local<OccupiedSpace>,
-    is_visible: Res<IsVisible>,
-    params: Res<DrawDockParams>,
-) {
-    let left_dock_widgets = &params.left_dock_widgets;
+fn draw_dock(mut contexts: EguiContexts, is_visible: Res<IsVisible>, params: Res<DrawDockParams>) {
     let ctx = contexts.ctx_mut();
-    if is_visible.left {
-        o_space.left = panel_builder(ctx, &left_dock_widgets, "left", "Left Panel".to_string()).x;
-    }
-    if is_visible.right {
-        o_space.right =
-            panel_builder(ctx, &left_dock_widgets, "right", "Right Panel".to_string()).x;
-    }
-    if is_visible.top {
-        o_space.top = panel_builder(ctx, &left_dock_widgets, "top", "Top Panel".to_string()).y;
-    }
-    if is_visible.bottom {
-        o_space.bottom = panel_builder(
-            ctx,
-            &left_dock_widgets,
-            "bottom",
-            "Bottom Panel".to_string(),
-        )
-        .y;
+    let mut o_space = OccupiedSpace::default();
+
+    for (panel_type, p_builder) in &params.panel_builders {
+        let p_label = format!("{} Panel", panel_type.to_uppercase());
+
+        if (panel_type == "left" && is_visible.left)
+            || (panel_type == "right" && is_visible.right)
+            || (panel_type == "top" && is_visible.top)
+            || (panel_type == "bottom" && is_visible.bottom)
+        {
+            let size = panel_builder(ctx, p_builder, panel_type, p_label);
+
+            match panel_type.as_str() {
+                "left" => {
+                    o_space.left = size.x;
+                }
+                "right" => {
+                    o_space.right = size.x;
+                }
+                "top" => {
+                    o_space.top = size.y;
+                }
+                "bottom" => {
+                    o_space.bottom = size.y;
+                }
+                _ => panic!("Invalid panel type {}", panel_type),
+            }
+        }
     }
 }
 
@@ -139,50 +143,30 @@ fn draw_dock(
 /// - p_label: String   // a string for the panel LABEL to build
 fn panel_builder(
     ctx: &mut Context,
-    left_dock_widgets: &Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>,
+    panel_builder: &Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>,
     p_type: &str,
     p_label: String,
 ) -> egui::Vec2 {
-    let panel_builder = match p_type {
-        "left" => PanelBuildType::Side(egui::SidePanel::left(p_label.clone())),
-        "right" => PanelBuildType::Side(egui::SidePanel::right(p_label.clone())),
-        "top" => PanelBuildType::TopBottom(egui::TopBottomPanel::top(p_label.clone())),
-        "bottom" => PanelBuildType::TopBottom(egui::TopBottomPanel::bottom(p_label.clone())),
-        _ => panic!("Invalid panel type"),
+    let panel_type = match p_type {
+        "left" => PanelBuildType::Side(egui::SidePanel::left(p_label).resizable(true)),
+        "right" => PanelBuildType::Side(egui::SidePanel::right(p_label).resizable(true)),
+        "top" => PanelBuildType::TopBottom(egui::TopBottomPanel::top(p_label).resizable(true)),
+        "bottom" => {
+            PanelBuildType::TopBottom(egui::TopBottomPanel::bottom(p_label).resizable(true))
+        }
+        _ => panic!("Invalid panel type {}", p_type),
     };
 
-    // match expression to create an egui::Response based on PanelBuildType enum
-    // -
-    let response = match panel_builder {
-        PanelBuildType::Side(side_panel) => {
-            side_panel
-                .resizable(true)
-                .show(ctx, |ui| {
-                    ui.label(&p_label);
-                    if p_type == "left" {
-                        left_dock_widgets(ui);
-                    }
-                    // allocate layout space for the panel
-                    ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover())
-                })
-                .response
-        }
-        PanelBuildType::TopBottom(top_bottom_panel) => {
-            top_bottom_panel
-                .resizable(true)
-                .show(ctx, |ui| {
-                    ui.label(&p_label);
-                    ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover())
-                })
-                .response
-        }
+    let ui = match panel_type {
+        PanelBuildType::Side(panel) => panel.show(ctx, |ui| {
+            // @note : Call the closure here:
+            panel_builder(ui);
+        }),
+        PanelBuildType::TopBottom(panel) => panel.show(ctx, |ui| {
+            // @note : Call the closure here:
+            panel_builder(ui);
+        }),
     };
 
-    // extract the RECT size of the panel from the response
-    // egui::Response represents the result of interacting with a user interface
-    // (UI) element. It contains information about the current state of the
-    // interaction, such as whether the element was clicked, hovered, or
-    // dragged, along with other details like the position and size of the
-    // element.
-    response.rect.size()
+    ui.response.rect.size()
 }
--- rusty_jeep_project/bevy_inspector_gui/examples/egui_playground.rs
+++ rusty_jeep_project/bevy_inspector_gui/examples/egui_playground.rs
@@ -1,16 +1,24 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::egui;
+use std::collections::HashMap;
+use std::sync::Arc;
 
 mod dock_plugin;
 use dock_plugin::DockPlugin;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(DockPlugin::new(|ui: &mut egui::Ui| {
-            ui.label("Right Corner of the Playground");
+    let mut panel_builders = HashMap::new();
+    panel_builders.insert(
+        "left".to_string(),
+        Arc::new(|ui: &mut egui::Ui| {
+            ui.label("Left Corner of the Playground");
             let mut is_checked = true;
             ui.checkbox(&mut is_checked, "Check Me Out in the Playground");
-        }))
+        }) as Arc<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>,
+    );
+    let dock_plugin = DockPlugin::new(panel_builders);
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(dock_plugin)
         .run();
 }
